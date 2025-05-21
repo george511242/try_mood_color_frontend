263437dc89a8d62047c138b4d8132ea458ac2b2e
george511242 <g20045cheng@gmail.com>
2025-05-21 20:33:30 +0800

    update

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,10 +1,15 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
+const { defineConfig } = require('@vue/cli-service');
 
+module.exports = defineConfig({
+  publicPath: '/',
+  outputDir: 'dist', // 預設已是 dist，可省略
+  transpileDependencies: true,
   pluginOptions: {
     vuetify: {
       // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
     },
   },
+  devServer: {
+    historyApiFallback: true
+  }
 });
