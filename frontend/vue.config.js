fa1f457df3e65eea5314554cc37d53a10a16ce3b
george511242 <g20045cheng@gmail.com>
2025-05-27 16:49:15 +0800

    update

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,10 +1,20 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
   transpileDependencies: true,
 
+  // 指定輸出目錄與 public 路徑
+  publicPath: '/',
+  outputDir: 'dist',
+
+  // 避免 history 模式造成的 404 問題
+  devServer: {
+    historyApiFallback: true,
+  },
+
   pluginOptions: {
     vuetify: {
-      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+      // vuetify 設定可維持不變
     },
   },
 });
